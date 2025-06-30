965ba3f9bbc680492d710ca613cda373f6a1bd0f
kien.nguyen <kien.nguyen@asnet.com.vn>
2025-06-30 15:37:02 +0700

    feat: update Babel presets for Expo and TypeScript support

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['babel-preset-expo', '@babel/preset-typescript'],
   plugins: [
     'react-native-reanimated/plugin',
     [
